8e6c8bc5a9c08c972e68d290d7953db000aff617
user03 <user03@example.com>
2025-01-24 15:38:27 +0900

    timezoneとlocaleを正しく設定(.envのデータベース接続設定は各自が記述必要)

--- config/app.php
+++ config/app.php
@@ -65,7 +65,8 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'UTC'),
+    // 'timezone' => env('APP_TIMEZONE', 'UTC'),
+    'timezone' => 'Asia/Tokyo',
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +79,8 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
+    // 'locale' => env('APP_LOCALE', 'en'),
+    'locale' => 'ja',
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
