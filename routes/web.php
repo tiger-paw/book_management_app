bf1099b7847d51edc0ae087241103f8c7f48f497
user03 <user03@example.com>
2025-01-27 16:58:44 +0900

    追加：ログインフォームを作成 追加：ORマッパーがu_idカラムを認識する記述を追加

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TopController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('index');
 });
 
 Route::get('/',[TopController::class,'index']);
+
+
+// loginページ
+Route::get('/login', function() {
+    return view('loginForm');
+});
+
+Route::post('/login', [AuthController::class, 'login']);
+
