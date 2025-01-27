6ebcb86b0ec22224fe66e4e4714f1d03ad64a903
takamagari <takamagari.rina@gmail.com>
2025-01-27 14:33:39 +0900

    メニュー画面を編集

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TopController;
+use App\Http\Controllers\BooksController;
 
+// ここで返されるビューは resources/views/index.blade.php
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
 
-Route::get('/',[TopController::class,'index']);
+// メニュー画面
+Route::get('/',[TopController::class,'index'])->name('index');
+
+// 書籍の一覧
+Route::get('/books',[BooksController::class,'index'])->name('books.index');
