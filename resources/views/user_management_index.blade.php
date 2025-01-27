f37ee97fd4d177e25328762e3d93f8dc2790efc6
user05 <user05@example.com>
2025-01-27 10:46:26 +0900

    社員管理のホームを作成

---
+++ resources/views/user_management_index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>社員管理</title>
+</head>
+<body>
+<h1>社員管理</h1>
+    <p>行いたい処理を選択してください</p>
+    <ul>
+        <li><a href="/db/index">全社員の一覧表示</a></li>
+        <li><a href="/db/create">社員の新規登録</a></li>
+        <li><a href="/db/erase">データの削除</a></li>
+    </ul>
+</body>
+</html>
