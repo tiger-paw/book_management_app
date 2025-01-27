275d5052c346b94094b671962ed60b8b2f5834dc
takamagari <takamagari.rina@gmail.com>
2025-01-27 09:45:50 +0900

    TopControllerを新規作成

---
+++ app/Http/Controllers/TopController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+// 利用するモデルの読み込み
+
+class TopController extends Controller
+{
+    
+    // メニュー画面
+    public function index()
+    {
+        // ビューを返す処理を記載
+    }
+}
