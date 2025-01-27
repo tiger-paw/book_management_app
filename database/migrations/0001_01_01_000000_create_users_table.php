5cdcfbe77d114aa45c5fbc31039a15d7cca63521
user03 <user03@example.com>
2025-01-27 11:49:03 +0900

    ボツ案のマイグレーションファイルを削除

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,38 +11,38 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('u_id')->autoIncrement()->primary(); // 追加
-            $table->string('u_name'); // 追加
-            $table->string('password'); // 追加
-            $table->integer('user_code'); // 追加
-            $table->foreignId('d_id')->constrained(); // 追加
-            $table->timestamps(); // 追加
+        // Schema::create('users', function (Blueprint $table) {
+            // $table->unsignedBigInteger('u_id')->autoIncrement()->primary(); // 追加
+            // $table->string('u_name'); // 追加
+            // $table->string('password'); // 追加
+            // $table->integer('user_code'); // 追加
+            // $table->foreignId('d_id')->constrained(); // 追加
+            // $table->timestamps(); // 追加
 
 
-            // $table->id();
-            // $table->string('name');
-            // $table->string('email')->unique();
-            // $table->timestamp('email_verified_at')->nullable();
-            // $table->string('password');
-            $table->rememberToken();
-            // $table->timestamps();
-        });
+            // // $table->id();
+            // // $table->string('name');
+            // // $table->string('email')->unique();
+            // // $table->timestamp('email_verified_at')->nullable();
+            // // $table->string('password');
+            // $table->rememberToken();
+            // // $table->timestamps();
+        // });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
+        // Schema::create('password_reset_tokens', function (Blueprint $table) {
+        //     $table->string('email')->primary();
+        //     $table->string('token');
+        //     $table->timestamp('created_at')->nullable();
+        // });
 
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
+        // Schema::create('sessions', function (Blueprint $table) {
+        //     $table->string('id')->primary();
+        //     $table->foreignId('user_id')->nullable()->index();
+        //     $table->string('ip_address', 45)->nullable();
+        //     $table->text('user_agent')->nullable();
+        //     $table->longText('payload');
+        //     $table->integer('last_activity')->index();
+        // });
     }
 
     /**
@@ -50,8 +50,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        // Schema::dropIfExists('users');
+        // Schema::dropIfExists('password_reset_tokens');
+        // Schema::dropIfExists('sessions');
     }
 };
