3b60d27db6b44781b35e5ffe5b5de888280ccd6d
user05 <user05@example.com>
2025-01-28 09:25:54 +0900

    仮の通信制御の作成

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TopController;
+use App\Http\Controllers\UsersController;
+
 
 Route::get('/', function () {
-    return view('index');
+    return view('user_management_index');
 });
 
-Route::get('/',[TopController::class,'index']);
+Route::get('/db/user_management_index',[UsersController::class,'index']);
+
+Route::get('/db/user_management_create',[UsersController::class,'create']);
+Route::post('/db/user_management_store',[UsersController::class,'store']);
+
+Route::get('/db/user_management_erase',[UsersController::class,'erase']);
+Route::post('/db/user_management_erase',[UsersController::class,'erase']);
+Route::post('/db/user_management_delete',[UsersController::class,'delete']);
