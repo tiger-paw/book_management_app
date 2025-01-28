cb215f79dc8872271c4e38ffeafaf95d115c2eff
user03 <user03@example.com>
2025-01-28 15:28:06 +0900

    書籍検索フォームの追加

---
+++ resources/views/search/book_search.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>書籍検索</title>
+</head>
+<body>
+    <h1>書籍検索</h1>
+    <p>キーワード（書籍名・著者名）</p>
+    <form action="/searchBooks" method="get">
+        <input type="text" name="search_books">
+    </form>
+</body>
+</html>
