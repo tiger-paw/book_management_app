c93282eb807c75b57645f613308fce578bbcd8b5
user05 <user05@example.com>
2025-01-30 10:06:32 +0900

    一旦

--- resources/views/user_management_index.blade.php
+++ resources/views/user_management_index.blade.php
@@ -1,25 +1,16 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>社員管理メニュー</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" 
-    crossorigin="anonymous">
-</head>
-<body>
+
+@extends('layouts.app')
+@section('title', '社員管理')
+@section('content')
+<div class="d-flex justify-content-end">
+        <a href="{{ route('index') }}" class="btn btn-secondary">メニューに戻る</a>
+    </div>
 <h1>社員管理画面</h1>
     <p>行いたい処理を選択してください</p>
     <nav aria-label="breadcrumb">
     <ul class="breadcrumb">
-        <li class="breadcrumb-item"><a href="/db/user_management_index">全社員の一覧表示</a></li>
-        <li class="breadcrumb-item"><a href="/db/user_management_create">社員の新規登録</a></li>
-        <li class="breadcrumb-item"><a href="/db/user_management_erase">社員の削除</a></li><br>
-        <a href="/">メニューに戻る</a>
+        <a href="/db/user_management_index" class="btn btn-secondary mx-2">全社員の一覧表示</a></li>
+        <a href="/db/user_management_create" class="btn btn-secondary mx-2">社員の新規登録</a></li>
+        <a href="/db/user_management_erase" class="btn btn-outline-danger mx-2">社員の削除</a></><br>
     </ul>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" 
-    crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection
