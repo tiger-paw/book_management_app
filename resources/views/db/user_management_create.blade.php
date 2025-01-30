12aec42f12e6e60959dd8d06de7892a8791ed301
user03 <user03@example.com>
2025-01-30 15:54:33 +0900

    [キャンセル]ボタンに統一されていない状態を戻るに統一した

--- resources/views/db/user_management_create.blade.php
+++ resources/views/db/user_management_create.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('title', '社員管理')
 @section('content')
-<div class="d-flex justify-content-end">
-    <a href="{{ route('index') }}" class="btn btn-secondary">メニューに戻る</a>
-</div>
-<h1>社員の新規登録</h1>
+    <div class="d-flex justify-content-end">
+        <a href="{{ route('index') }}" class="btn btn-secondary">メニューに戻る</a>
+    </div>
+    <h1>社員の新規登録</h1>
 
     <form action="/db/user_management_store" method="post">
         @csrf
@@ -13,14 +13,14 @@
         <p>部署ID：<input type="number" name="d_id" required></p>
         <p>社員コード：<input type="text" name="user_code" required></p>
         <input type="submit" value="登録" class="btn btn-primary">
-        <a href="{{ route('user_management.index') }}" class="btn btn-secondary">キャンセル</a>   
+        <a href="{{ route('user_management.index') }}" class="btn btn-secondary">戻る</a>
     </form>
     <br>
-    @if($errors->any())
+    @if ($errors->any())
         <ul>
-            @foreach($errors->all() as $error)
-            <li>{{ $error }}</li>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
             @endforeach
         </ul>
-    @endif    
+    @endif
 @endsection
