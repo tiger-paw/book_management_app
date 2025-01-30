2d9ec1ad639300c363c93e100cfe919be7e7955c
user05 <user05@example.com>
2025-01-30 13:14:41 +0900

    一旦

--- resources/views/db/user_management_erase.blade.php
+++ resources/views/db/user_management_erase.blade.php
@@ -10,10 +10,10 @@
     <form action="/db/user_management_delete" method="post">
             @csrf
             <p>以下のデータを削除します　よろしいですか？</p>
-            社員番号:<input type="text" name="u_id" value="{{ $record ->u_id }}" readonly><br>
-            社員名:<input type="text" name="u_name" value="{{ $record ->u_name }}" readonly><br>
-            部署ID:<input type="text" name="d_id" value="{{ $record ->d_id }}" readonly><br>
-            社員コード:<input type="text" name="user_code" value="{{ $record ->user_code }}" readonly><br>
+            社員番号：<input type="text" name="u_id" value="{{ $record ->u_id }}" readonly><br>
+            社員名：<input type="text" name="u_name" value="{{ $record ->u_name }}" readonly><br>
+            部署ID：<input type="text" name="d_id" value="{{ $record ->d_id }}" readonly><br>
+            社員コード：<input type="text" name="user_code" value="{{ $record ->user_code }}" readonly><br>
 
             <input type="submit" value="削除" class="btn btn-outline-danger"><br>
             <a href="/db/user_management_erase" class="btn btn-outline-primary">キャンセル</a><br>
@@ -23,7 +23,7 @@
     @else
         <form action="/db/user_management_erase" method="post">
         @csrf  
-        社員番号:<input type="number" name="u_id" required><br>
+        社員番号：<input type="number" name="u_id" required><br>
             <input type="submit" value="確認" class="btn btn-outline-primary"><br>
             <a href="{{ route('user_management.index') }}" class="btn btn-outline-primary">キャンセル</a>
 
