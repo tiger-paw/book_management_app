7843e1f0d0a60e468f6d0e01fc7de6c4b5b2d75e
user05 <user05@example.com>
2025-01-30 10:55:53 +0900

    コミット

--- resources/views/db/user_management_create.blade.php
+++ resources/views/db/user_management_create.blade.php
@@ -1,24 +1,19 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" 
-    crossorigin="anonymous">
-</head>
-<body>
+@extends('layouts.app')
+@section('title', '社員管理')
+@section('content')
+<div class="d-flex justify-content-end">
+    <a href="{{ route('index') }}" class="btn btn-secondary">メニューに戻る</a>
+</div>
 <h1>社員の新規登録</h1>
-<a href="{{ route('user_management.index') }}">社員管理画面に戻る</a>
-    
+
     <form action="/db/user_management_store" method="post">
         @csrf
-        <p>社員名：<input type="text" name="u_name" class="navbar-brand mb-0 h1"required></p>
+        <p>社員名：<input type="text" name="u_name" required></p>
         <p>パスワード：<input type="password" name="password" required></p>
         <p>部署ID：<input type="number" name="d_id" required></p>
         <p>社員コード：<input type="text" name="user_code" required></p>
-        <input type="submit" value="確認" class="btn btn-primary">    
+        <input type="submit" value="登録" class="btn btn-primary"> 
+        <a href="{{ route('user_management.index') }}" class="btn btn-secondary">キャンセル</a>   
     </form>
     <br>
     @if($errors->any())
@@ -28,8 +23,4 @@
             @endforeach
         </ul>
     @endif    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" 
-    crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection
--- resources/views/db/user_management_erase.blade.php
+++ resources/views/db/user_management_erase.blade.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>社員削除</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" 
-    crossorigin="anonymous">
-</head>
+@extends('layouts.app')
+@section('title', '社員管理')
+@section('content')
+<div class="d-flex justify-content-end">
+    <a href="{{ route('index') }}" class="btn btn-secondary">メニューに戻る</a>
+</div>
 <body>
     <h1>データの削除</h1>
-    <a href="{{ route('user_management.index') }}">社員管理画面に戻る</a>
     @if(isset($record))
     <form action="/db/user_management_delete" method="post">
             @csrf
@@ -20,8 +15,10 @@
             部署ID:<input type="text" name="d_id" value="{{ $record ->d_id }}" readonly><br>
             社員コード:<input type="text" name="user_code" value="{{ $record ->user_code }}" readonly><br>
 
-            <input type="submit" value="削除"><br>
-            <a href="/db/user_management_erase">社員削除画面に戻る</a>
+            <input type="submit" value="削除" class="btn btn-outline-danger"><br>
+            <a href="/db/user_management_erase" class="btn btn-secondary">キャンセル</a>
+            <a href="{{ route('user_management.index') }}" class="btn btn-secondary">社員管理画面に戻る</a>
+
         </form>
     @else
         <form action="/db/user_management_erase" method="post">
@@ -30,8 +27,4 @@
             <input type="submit" value="確認" class="btn btn-primary">
         </form>
     @endif
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" 
-    crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection
