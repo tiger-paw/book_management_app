3d0243028ec35fe1a266d87ab9fae903c0db10fe
takamagari <takamagari.rina@gmail.com>
2025-01-30 14:10:42 +0900

    書籍検索、検索結果画面のボタンを変更

--- resources/views/search/search_books_form.blade.php
+++ resources/views/search/search_books_form.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 @section('title', '書籍検索')
 @section('content')
+    <div class="d-flex justify-content-end">
+        <a href="{{ route('index') }}" class="btn btn-secondary">メニューに戻る</a>
+    </div>
     <h1>書籍検索</h1>
     <p>キーワード（書籍名・著者名）</p>
     <form action="/search_books_result" method="get">
@@ -9,7 +12,8 @@
             <input type="text" name="book_keyword" required>
         </div>
         <div>
-            <input type="submit" value="検索">
+            <br>
+            <input type="submit" class="btn btn-primary" value="検索">
         </div>
     </form>
 </body>
