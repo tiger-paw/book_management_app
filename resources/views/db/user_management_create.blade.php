5ba18dc0c298bcb701ead26dab6729734ce62c7f
user03 <user03@example.com>
2025-01-31 09:35:57 +0900

    社員登録フォームの修正したが、ずれが大きくなったので元に戻した

--- resources/views/db/user_management_create.blade.php
+++ resources/views/db/user_management_create.blade.php
@@ -9,9 +9,9 @@
 
     <form action="/db/user_management_store" method="post">
         @csrf
-        <p style="margin-left: 2rem;">社員名：<input type="text" name="u_name" required></p>
+        <p>社員名：<input type="text" name="u_name" required></p>
         <p>パスワード：<input type="password" name="password" required></p>
-        <p style="margin-left: 2rem;">部署ID：<input type="number" name="d_id" required></p>
+        <p>部署ID：<input type="number" name="d_id" required></p>
         <p>社員コード：<input type="text" name="user_code" required></p>
         <input type="submit" value="登録" class="btn btn-primary">
         <a href="{{ route('user_management.index') }}" class="btn btn-secondary">戻る</a>
